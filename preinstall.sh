346c6be585b660f253c457dadede21c8b4f219a6
Mark Allen <mark@markcallen.com>
2015-08-23 12:25:39 -0700

    Added checks for vpc and cidrblock.

--- preinstall.sh
+++ preinstall.sh
@@ -35,8 +35,9 @@ fi
 
 HOSTNAME=uhostserver
 CIDR_BLOCK="10.0.0.0/28"
-AMI=ami-a9e2da99
-INSTANCE_TYPE=t2.micro
+AMI=ami-afd5c09f # get ami list from http://cloud-images.ubuntu.com/locator/ec2/
+INSTANCE_TYPE=t2.small # requires hvm ami
+VOLUME_SIZE=20
 
 while getopts h:n:a:i: option
 do
@@ -53,44 +54,77 @@ VPC_NAME=${HOSTNAME}-vpc
 SECURITY_GROUP=${HOSTNAME}-security-group
 KEY_NAME=${HOSTNAME}-key
 
-echo "Creating VPC $VPC_NAME ($CIDR_BLOCK)"
+echo "Checking to see if ${CIDR_BLOCK} is already in use"
+vpcId=`aws ec2 describe-vpcs --filter Name=cidr,Values=${CIDR_BLOCK} --query 'Vpcs[*].VpcId' --output text`
+if [ ! -z "$vpcId" ]; then
+  echo "Found ${vpcId} using ${CIDR_BLOCK}.  Exitting!!"
+  exit 1;
+fi
 
-vpcId=`aws ec2 create-vpc --cidr-block $CIDR_BLOCK --query 'Vpc.VpcId' --output text`
+echo "Checking to see if keypair ${KEY_NAME} already exists"
+keypair=`aws ec2 describe-key-pairs --filter Name=key-name,Values=${KEY_NAME} --query 'KeyPairs[*].KeyName' --output text`
+if [ ! -z "$keypair" ]; then
+  echo "${KEY_NAME} already in use: Exitting!!"
+  exit 1;
+fi
+
+vpcId=`aws ec2 describe-vpcs --filter Name=tag:Name,Values=${VPC_NAME} --query 'Vpcs[*].VpcId'`
+
+if [ ! -z "$vpcId" ]; then
+  echo "Using Existing vpc-id: $vpcId"
+else
+  echo "Creating VPC ${VPC_NAME}: ${CIDR_BLOCK}"
+  vpcId=`aws ec2 create-vpc --cidr-block $CIDR_BLOCK --query 'Vpc.VpcId' --output text`
+fi
 
 aws ec2 modify-vpc-attribute --vpc-id $vpcId --enable-dns-support "{\"Value\":true}"
 aws ec2 modify-vpc-attribute --vpc-id $vpcId --enable-dns-hostnames "{\"Value\":true}"
 aws ec2 create-tags --resources $vpcId --tag "Key=Name,Value=${VPC_NAME}"
 
+echo "Creating Internet Gateway"
 internetGatewayId=`aws ec2 create-internet-gateway --query 'InternetGateway.InternetGatewayId' --output text`
 aws ec2 attach-internet-gateway --internet-gateway-id $internetGatewayId --vpc-id $vpcId
 aws ec2 create-tags --resources $internetGatewayId --tag "Key=Name,Value=${VPC_NAME}-internetgateway"
 
+echo "Creating Subnet"
 subnetId=`aws ec2 create-subnet --vpc-id $vpcId --cidr-block $CIDR_BLOCK --query 'Subnet.SubnetId' --output text`
 aws ec2 create-tags --resources $subnetId --tag "Key=Name,Value=${VPC_NAME}-subnet"
 
+echo "Creating Routing Table"
 routeTableId=`aws ec2 create-route-table --vpc-id $vpcId --query 'RouteTable.RouteTableId' --output text`
-aws ec2 associate-route-table --route-table-id $routeTableId --subnet-id $subnetId
-aws ec2 create-route --route-table-id $routeTableId --destination-cidr-block 0.0.0.0/0 --gateway-id $internetGatewayId
+aws ec2 associate-route-table --route-table-id $routeTableId --subnet-id $subnetId --output text
+aws ec2 create-route --route-table-id $routeTableId --destination-cidr-block 0.0.0.0/0 --gateway-id $internetGatewayId --output text
 aws ec2 create-tags --resources $routeTableId --tag "Key=Name,Value=${VPC_NAME}-routetable"
 
 echo "Created VPC: $vpcId"
 
 securityGroupId=`aws ec2 create-security-group --group-name $SECURITY_GROUP --description "$SECURITY_GROUP" --vpc-id $vpcId --query 'GroupId' --output text`
 aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 22 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 80 --cidr 0.0.0.0/0
+aws ec2 authorize-security-group-ingress --group-id $securityGroupId --protocol tcp --port 443 --cidr 0.0.0.0/0
+aws ec2 create-tags --resources $securityGroupId --tag "Key=Name,Value=${SECURITY_GROUP}"
 
 echo "Create Security Group: $SECURITY_GROUP ($securityGroupId)"
 
+if [ -f ${KEY_NAME}.pem ]; then
+  rm -f ${KEY_NAME}.pem
+fi
 aws ec2 create-key-pair --key-name $KEY_NAME --query 'KeyMaterial' --output text > ./${KEY_NAME}.pem
 chmod 400 ${KEY_NAME}.pem
 
 echo "Created Key: $KEY_NAME saved as ${KEY_NAME}.pem"
 
-instanceId=`aws ec2 run-instances --image-id $AMI --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME  --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --query 'Instances[0].InstanceId' --output text`
-instanceUrl=`aws ec2 describe-instances --instance-ids $instanceId --query 'Reservations[0].Instances[0].PublicDnsName' --output text`
+instanceId=`aws ec2 run-instances --image-id $AMI --count 1 --instance-type $INSTANCE_TYPE --key-name $KEY_NAME  --security-group-ids $securityGroupId --subnet-id $subnetId --associate-public-ip-address --block-device-mapping "[ { \"DeviceName\": \"/dev/sda1\", \"Ebs\": { \"VolumeSize\": $VOLUME_SIZE } } ]" --query 'Instances[0].InstanceId' --output text`
 aws ec2 create-tags --resources $instanceId --tag "Key=Name,Value=$HOSTNAME"
+sleep 10
+instanceUrl=`aws ec2 describe-instances --instance-ids $instanceId --query 'Reservations[0].Instances[0].PublicDnsName' --output text`
 
 echo "Created instance $instanceUrl ($instanceId)"
 
 echo "Connect using: ssh -i ${KEY_NAME}.pem ubuntu@$instanceUrl"
 
+echo "Download: wget https://raw.githubusercontent.com/uhost/uhostserver/master/installserver.sh"
+echo "Install: sudo bash ./installserver.sh -n <site name>"
+
+echo "Then test using: rake spec TARGET_HOST=$instanceUrl IDENTITY_FILE=${KEY_NAME}.pem"
 
